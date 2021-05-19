d96229307843bb6dbe1cf5904a66691f0e0fd866
loitepr <preben.loite@rs.no>
2021-05-19 22:55:49 +0200

    img swap ok, ios test now

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
-function test() {
-
+function fChangeImage(iImage) {
+  if (iImage.src.includes("_on.png")) {
+    iImage.src = iImage.src.replace("_on", "_off"); }
+  else if (iImage.src.includes("_off.png")) {
+    iImage.src = iImage.src.replace("_off", "_on"); }
 };
